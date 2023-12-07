8ba737dcdb6a320f3e8d1565a0a85151f296b006
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2023-12-07 14:41:16 +0000

    upload 1 - 4

---
+++ solutions/Ch_04_Solutions/Ch_04_Projects/chp4Notes.py
@@ -0,0 +1,32 @@
+# data = "Hi there!"
+# for index in range(len(data)):
+#     print(index, data[index])
+
+# anim1 = "zebra"
+# anim2 = "lion"
+# print(anim1[0], anim1[1], anim1[2], anim1[4])
+# print(anim2[-1], anim2[-2], anim2[-4])
+
+# s1 = "abracadabra"
+# print(s1[4:7])
+# print(s1[:4])
+# print(s1[7:])
+# print(s1[::3])
+# print(s1[::-1])
+
+# print("Testing substring with the in Operator")
+
+# namelist = ["Brendan Bombard", "Quintin Blake", "Calvin Cordes", "Wade Miller", "Tim Frick", "Mukenz Sagriff"]
+
+# for name in namelist:
+#     if "Sagriff" in name:
+#         print(name + " needs to learn how to spell her name.")
+#         print("Ummmmm duuuuuhhhh, Mackenzie")
+
+print("The in Operator:")
+
+print("at" in "Catapult") # True
+print(35 in [12, 33, 37, 32, 91]) # False
+print(35 in {'x': 35, 'y' : 21}) # False
+print(35 in {35: 'x', 21: 'y'}) # True
+
---
+++ solutions/Ch_04_Solutions/Ch_04_Projects/chp4Test.py
@@ -0,0 +1,5 @@
+myString="this is my string"
+reverseString=" "
+for index in range(len(myString)):
+    reverseString=index,myString[index::-1]
+    print(reverseString)
