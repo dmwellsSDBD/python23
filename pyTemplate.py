1b3fc71c8e43803e010b828b136097a12c9bcbc5
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2023-12-07 09:35:16 -0500

    uploaded more files

---
+++ pyTemplate.py
@@ -0,0 +1,23 @@
+"""
+Docstring Goes here...
+Program Name:
+Author(2) Name:
+Program Purpose:
+Program Layout (Pseudo-code):
+"""
+# imports go here:
+
+
+# Initialize any CONSTANTS
+
+
+# Initialize any variables
+
+
+# Prompt for input
+
+
+# Process data into information
+
+
+# Display Results
---
+++ testCode.py
@@ -0,0 +1,4 @@
+text = "apple, banana, apple, cherry"
+fruits = text.split(", ")
+count_apples = fruits.count("apple")
+print("Number of apples:", count_apples) 
